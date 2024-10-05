4a6f0665c782c64dc89714f864da6fa8940314cf
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-10-06 01:28:45 +0600

    applied try catch on post user api

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,24 +2,42 @@ const { db } = require("../utils/dbconnection");
 
 const usersCollection = db.collection("users");
 
+//get all users
 const getUsers = async (req, res) => {
   const result = await usersCollection.find().toArray();
   res.send(result);
 };
 
+//get a specific user
 const getSingleUser = async (req, res) => {};
 
+//post a user
 const postUser = async (req, res) => {
   const user = req.body;
   const query = { email: user.email };
-  const exist = await usersCollection.findOne(query);
 
-  if (exist) {
-    return res.send({ message: "user already exists" });
+  try {
+    // Check if the user already exists
+    const exist = await usersCollection.findOne(query);
+    if (exist) {
+      return res.status(409).send({ message: "User already exists" });
+    }
+    // Insert new user
+    const result = await usersCollection.insertOne(user);
+
+    if (result.insertedId) {
+      res.status(201).send({
+        message: "User created successfully",
+        userId: result.insertedId,
+      });
+    } else {
+      res.status(500).send({ message: "Failed to create user" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "An error occurred", error: error.message });
   }
-
-  const result = await usersCollection.insertOne(user);
-  res.send(result);
 };
 
 module.exports = { getUsers, getSingleUser, postUser };
