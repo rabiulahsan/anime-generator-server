a1870c471855ad54711ad062a06f12c147dc2f54
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-10-06 02:26:17 +0600

    make some change for good prompt

--- utils/getImageData.js
+++ utils/getImageData.js
@@ -1,6 +1,7 @@
 const getImageData = async (prompt) => {
+  const modifiedPrompt = `Create a realistic anime image based on: ${prompt}`;
   const form = new FormData();
-  form.append("prompt", prompt);
+  form.append("prompt", modifiedPrompt);
   const response = await fetch("https://clipdrop-api.co/text-to-image/v1", {
     method: "POST",
     headers: {
