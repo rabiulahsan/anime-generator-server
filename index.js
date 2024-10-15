5e2bc69b651b3cb533321326927965f5d3ae402c
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-10-15 12:22:29 +0600

    create api for payment

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/users.route");
 const animeRoutes = require("./routes/animies.route");
 const jwtRoutes = require("./routes/jwt.route");
 const verifyJWT = require("./utils/verifyJWT");
+const stripe = require("stripe")(process.env.Stripe_Key);
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -24,6 +25,21 @@ app.use("/jwt", jwtRoutes);
 //test
 app.get("/", (req, res) => {
   res.send("Running");
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { price } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: parseInt(price * 100),
+    currency: "usd",
+    payment_method_types: ["card"],
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
 });
 
 // Protected route
